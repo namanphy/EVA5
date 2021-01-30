a3a52d0c20058c39ccff034ceaffe851d143026c
namanphy <namanphy@gmail.com>
2021-01-30 22:11:12 +0530

    added lr step scheduler

--- S8/model/functions.py
+++ S8/model/functions.py
@@ -1,5 +1,6 @@
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.optim.lr_scheduler import StepLR
 
 
 def sgd_optimizer(model, lr=0.01, l2_factor=0, momentum=0.9):
@@ -16,3 +17,9 @@ def calculate_l1_loss(model, loss, lambda_l1=0.001):
 
 def cross_entropy_loss():
     return F.cross_entropy
+
+
+def step_lr_scheduler(optimizer, step_size=None, gamma=0.15):
+    if step_size is None:
+        raise Exception('step size value must be provided with valid integer')
+    return StepLR(optimizer, step_size=step_size, gamma=gamma)
