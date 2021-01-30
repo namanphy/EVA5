9abb9900dfd9a98397aac4994c3f4d8043b49545
namanphy <namanphy@gmail.com>
2021-01-30 19:13:06 +0530

    added augmentations - rotation & HorizontalFlip

--- S8/data/dataset.py
+++ S8/data/dataset.py
@@ -5,14 +5,20 @@ def cifar10_dataset(location='../data', train=True, download=True, transform=Non
     return datasets.CIFAR10(location, train=train, download=download, transform=transform)
 
 
-def transformations(augmentation=False, rotation=3.0):
+def transformations(augmentation=False, rotation=0.0, randomHorizontalFlip=False):
     transforms_list = [
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
+
+    augmentation_list = []
+    if randomHorizontalFlip:
+        augmentation_list.append(transforms.RandomHorizontalFlip())
+
+    if rotation != 0.0:
+        augmentation_list.append(transforms.RandomRotation((-rotation, rotation), fill=(1,)))
+
     if augmentation:
-        transforms_list = [
-                              transforms.RandomRotation((-rotation, rotation), fill=(1,))
-                          ] + transforms_list
+        transforms_list = transforms_list + augmentation_list
 
     return transforms.Compose(transforms_list)
