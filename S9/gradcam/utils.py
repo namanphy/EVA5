de08dddd029731e8e37211c83f697897e1b9f400
namanphy <namanphy@gmail.com>
2021-02-06 12:41:15 +0530

    utility func in one file

--- S9/gradcam/utils.py
+++ S9/gradcam/utils.py
@@ -1,13 +1,10 @@
 import cv2
-import torch
 import matplotlib.cm as cm
 import numpy as np
-from gradcam.gradcam import GradCAM
-from cuda import enable_cuda
 from torchvision import transforms
 
 
-def _save_gradcam(filename, gcam, raw_image):
+def save_gradcam(filename, gcam, raw_image):
     gcam = gcam.cpu().numpy()
     cmap = cm.jet_r(gcam)[..., :3] * 255.0
     gcam = (cmap.astype(np.float) + raw_image.astype(np.float)) / 2
@@ -32,7 +29,7 @@ def _preprocess(image_path, mean=None, std=None, input_size=None):
     return image, original_image
 
 
-def _load_images(image_path, **kwargs):
+def load_images(image_path, **kwargs):
 
     mean = kwargs['mean'] if 'mean' in kwargs else None
     std = kwargs['std'] if 'std' in kwargs else None
@@ -46,29 +43,3 @@ def _load_images(image_path, **kwargs):
     return images, raw_images
 
 
-def plot_gradcam(image_path, model, path_to_model, layer, classes=None, **kwargs):
-    device = enable_cuda()
-
-    image, original_image = _load_images(image_path, **kwargs)
-    image = torch.stack(image).to(device)
-
-    model.load_state_dict(torch.load(path_to_model))
-    model.to(device)
-    model.eval()
-
-    gcam = GradCAM(model=model, candidate_layers=layer)
-    probs, ids = gcam.forward(image, original_image[0])
-
-    gcam.backward(ids=ids[:, [0]])
-    region = gcam.generate(target_layer=layer)
-
-    print(f"\t #GRADCAM: {classes[ids[0, 0]] if classes else ids[0, 0]} ({probs[0, 0]})")
-
-    # Grad-CAM save
-    _save_gradcam(
-        filename=f"gradcam-{model.__class__.__name__}-{layer}-{classes[ids[0, 0]] if classes else ids[0, 0]}.png",
-        gcam=region[0, 0],
-        raw_image=original_image[0]
-    )
-
-    gcam.remove_hook()
