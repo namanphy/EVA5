8e5c2b08305757afc719197351fd19c287ea0da0
namanphy <namanphy@gmail.com>
2021-02-19 18:46:33 +0530

    fix pixel clipping - convert from (-1,1) to (0,255)

--- S10/utils.py
+++ S10/utils.py
@@ -87,7 +87,7 @@ def plot_results(data, classes=None):
             row_count += 1
         axs[row_count][idx % 5].axis('off')
         axs[row_count][idx % 5].set_title(f'Label: {label}\nPrediction: {prediction}')
-        axs[row_count][idx % 5].imshow(np.transpose(result['image'], (1, 2, 0)))
+        axs[row_count][idx % 5].imshow(np.uint(((np.transpose(result['image'], (1, 2, 0)) + 1)*255) / 2))
 
     plt.show()
     fig.savefig(f'incorrect_predictions.png', bbox_inches='tight')
