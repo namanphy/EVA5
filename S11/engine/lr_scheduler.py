7713a1c8c8c0879313ec21a4240387d300d43ae3
namanphy <namanphy@gmail.com>
2021-03-05 16:43:26 +0530

    one cycle lr

--- S11/engine/lr_scheduler.py
+++ S11/engine/lr_scheduler.py
@@ -1,4 +1,4 @@
-from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, OneCycleLR
 
 
 def step_lr_scheduler(optimizer, step_size=None, gamma=0.15):
@@ -9,3 +9,9 @@ def step_lr_scheduler(optimizer, step_size=None, gamma=0.15):
 
 def reduce_lr_on_plateau(optimizer, mode='min', factor=0.5, patience=2, verbose=False, min_lr=0):
     return ReduceLROnPlateau(optimizer, mode=mode, factor=factor, patience=patience, verbose=verbose, min_lr=min_lr)
+
+
+def once_cycle_lr(optimizer, max_lr, total_steps=None, epochs=None, steps_per_epoch=None, pct_start=0.3, div_factor=25.0):
+    return OneCycleLR(optimizer, max_lr, total_steps=total_steps, epochs=epochs, steps_per_epoch=steps_per_epoch,
+                      pct_start=pct_start, div_factor=div_factor, final_div_factor=10000.0, last_epoch=-1,
+                      verbose=False)
