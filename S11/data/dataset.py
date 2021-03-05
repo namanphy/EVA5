b7f6a401d32d5f55fee4e52ecc517dbe2c2f6dcf
namanphy <namanphy@gmail.com>
2021-03-05 16:33:54 +0530

    new transform for datatset

--- S11/data/dataset.py
+++ S11/data/dataset.py
@@ -4,12 +4,15 @@ import numpy as np
 
 
 class CIFAR10:
-    def __init__(self, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), rotation=0, horizontal_flip=0.0,
-                 cutout=0.0, cutout_hw_ratio=2):
+    def __init__(self, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), padding=(0, 0), crop=(0, 0),
+                 rotation=0, horizontal_flip=0.0, vertical_flip=0.0, cutout=0.0, cutout_hw_ratio=2):
         self.mean = mean
         self.std = std
+        self.padding = padding
+        self.crop = crop
         self.rotation = rotation  # [int, int] or int
         self.horizontal_flip = horizontal_flip
+        self.vertical_flip = vertical_flip
         self.cutout = cutout
         self.cutout_hw_ratio = cutout_hw_ratio
 
@@ -27,8 +30,11 @@ class CIFAR10:
             'train': train,
             'mean': self.mean,
             'std': self.std,
+            'padding': self.padding,
+            'crop': self.crop,
             'rotation': self.rotation,
             'horizontal_flip': self.horizontal_flip,
+            'vertical_flip': self.vertical_flip,
             'cutout': self.cutout,
             'cutout_height': self.image_size[1] // self.cutout_hw_ratio,
             'cutout_width': self.image_size[2] // self.cutout_hw_ratio
