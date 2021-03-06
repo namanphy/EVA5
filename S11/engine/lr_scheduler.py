efbef627a8bee38c06c8df34d20d99c4153f4155
namanphy <namanphy@gmail.com>
2021-03-06 22:10:20 +0530

    final div factor

--- S11/engine/lr_scheduler.py
+++ S11/engine/lr_scheduler.py
@@ -11,7 +11,8 @@ def reduce_lr_on_plateau(optimizer, mode='min', factor=0.5, patience=2, verbose=
     return ReduceLROnPlateau(optimizer, mode=mode, factor=factor, patience=patience, verbose=verbose, min_lr=min_lr)
 
 
-def once_cycle_lr(optimizer, max_lr, total_steps=None, epochs=None, steps_per_epoch=None, pct_start=0.3, div_factor=25.0):
+def once_cycle_lr(optimizer, max_lr, total_steps=None, epochs=None, steps_per_epoch=None, pct_start=0.3, div_factor=25.0,
+                  final_div_factor=10000.0):
     return OneCycleLR(optimizer, max_lr, total_steps=total_steps, epochs=epochs, steps_per_epoch=steps_per_epoch,
-                      pct_start=pct_start, div_factor=div_factor, final_div_factor=10000.0, last_epoch=-1,
+                      pct_start=pct_start, div_factor=div_factor, final_div_factor=final_div_factor, last_epoch=-1,
                       verbose=False)
