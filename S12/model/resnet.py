a18f54d2576d5f722e46fa5ec967d43e1042d6b6
namanphy <namanphy@gmail.com>
2021-03-13 12:32:20 +0530

    fix GAP layer dim - used AdaptiveAvgPool2d

--- S12/model/resnet.py
+++ S12/model/resnet.py
@@ -75,6 +75,7 @@ class ResNet(BaseModel):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.gap = nn.AdaptiveAvgPool2d(1)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -90,7 +91,7 @@ class ResNet(BaseModel):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = self.gap(out)
         out = out.view(out.size(0), -1)
         out = self.linear(out)  # Using linear instead of F.log_softmax(x).
         return out
